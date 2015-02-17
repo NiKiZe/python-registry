ce8491ab67025b661c1ba0a00d8c4d60f18c2ff0
Christian Nilsson <nikize@gmail.com>
2015-02-17 08:45:40 +0100

    Add test files from default windows efi BCD file.

---
+++ tests/resources/BCDefi.cmd
@@ -0,0 +1,10 @@
+reg load HKLM\BCD BCDefi
+reg export HKLM\BCD BCDefi.reg
+reg save HKLM\BCD BCDefi.sav
+reg unload HKLM\BCD
+
+reg load HKLM\BCD BCDefi.cpy
+reg export HKLM\BCD BCDefi.cpy.reg
+reg save HKLM\BCD BCDefi.cpy.sav
+reg unload HKLM\BCD
+
---
+++ tests/resources/readme.txt
@@ -0,0 +1,6 @@
+BCDefi is orignal file
+.cpy is copy of only used parts
+.unload is files after unloading (modified from original files)
+.sav is files created using reg save
+
+see BCDefi.cmd how .reg, .sav and .unload events happen
